ed2699396b25bb2faac0c323aa780118da2793d3
Godfrey Dekera <dekewgodfrey@gmail.com>
2023-07-25 04:04:27 -0700

    fixing the is_upper function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - function that checks for uppercase character.
+ * @c: character to be checked
+ * Return:  1 if c is uppercase
+ * 0 otherwise
  */
-int main(void)
-{
-    char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
